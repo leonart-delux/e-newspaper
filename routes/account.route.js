eb0a9b3562e80a76d66c99cb9599a76b6e0c98ff
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-12-16 04:15:35 +0700

    update account.route.js

--- routes/account.route.js
+++ routes/account.route.js
@@ -1,28 +1,41 @@
-import express from "express";
+import express, {json} from "express";
 import accountService from "../services/accountService.js";
 import helper from "../utils/helper.js";
 import moment from "moment";
+import bcrypt from 'bcryptjs'
 
-const userId = 4;
+const userId = 5;
 
 const router = express.Router();
 
 router.get('/', async function (req, res) {
     const user = await accountService.getVipStatus(userId);
-    req.session.user = accountService.getWriterPseudonym(user);
+
+    user.birth_date = moment(user.birth_date).format("DD-MM-YYYY");
+    req.session.user = await accountService.getWriterPseudonym(user);
+
     res.redirect('account/information');
 });
+
 
 router.get('/information', async function (req, res) {
     //Đáng lẽ là lấy từ session nhưng tạm thời lấy từ db đã
 
     const user = req.session.user;
-
     res.render('vwAccount/information', {
         layout: 'account',
         information: true,
         user: user,
     });
+});
+
+router.post('/information/update-pseudonym', async function (req, res) {
+    const userId = req.session.user.id;
+    const entity = {
+        pseudonym: req.body.pseudonym,
+    }
+    const ret = await accountService.updatePseudonym(userId, entity);
+    res.redirect('/account');
 });
 
 router.get('/premium', function (req, res) {
@@ -74,5 +87,60 @@ router.post('/premium', async function (req, res) {
         premium: true,
         user: user,
     });
+});
+
+router.post('/information/update-name', async function (req, res) {
+    const name = req.body.name;
+    const user = req.session.user;
+    const entity = {name: name}
+    await accountService.updateUser(user.id, entity);
+    res.redirect('/account');
+});
+
+router.post('/information/update-email', async function (req, res) {
+    const email = req.body.email;
+    const user = req.session.user;
+    const entity = {email: email}
+    await accountService.updateUser(user.id, entity);
+    res.redirect('/account');
+});
+
+router.post('/information/update-birth-date', async function (req, res) {
+    let dob = req.body.dob;
+    console.log(dob);
+    const user = req.session.user;
+    dob = moment(dob, 'DD-MM-YYYY').format("YYYY-MM-DD");
+    const entity = {birth_date: dob}
+    await accountService.updateUser(user.id, entity);
+    res.redirect('/account');
+});
+
+router.post('/information/update-password', async function (req, res) {
+    const password = req.body.newPassword;
+    const hashedPassword = bcrypt.hashSync(password, 8);
+    const user = req.session.user;
+    const entity = {password: hashedPassword}
+    console.log(entity);
+    await accountService.updateUser(user.id, entity);
+    res.redirect('/account');
+});
+
+router.post('/is-valid-password', function (req, res) {
+    const password = req.body.password;
+    const user = req.session.user;
+    if (bcrypt.compareSync(password, user.password)) {
+        return res.json(true);
+    }
+    return res.json(false);
+
+});
+
+router.get('/is-available-email', async function (req, res) {
+    const email = req.query.email;
+    const user = await accountService.isAvailableEmail(email);
+    if (!user) {
+        return res.json(true);
+    }
+    return res.json(false);
 });
 export default router;
