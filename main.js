f09fa63dc288ce56e15007c253f4a0abfeb23faa
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-12-15 11:05:20 +0700

    Add livereload, nodemon

---
+++ main.js
@@ -0,0 +1,52 @@
+import connectLiveReload from 'connect-livereload'
+import hbs_section from 'express-handlebars-sections';
+import livereload from 'livereload';
+import session from "express-session";
+import express from "express";
+import {engine} from "express-handlebars";
+import accountRoute from "./routes/account.route.js";
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 1);
+});
+
+var app = express();
+
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+    secret: 'SECRET_KEY',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}))
+
+app.use('/static', express.static('static'));
+app.engine('hbs', engine({
+    extname: 'hbs',
+    // defaultLayout: 'main',
+    helpers: {
+        format_number(value) {
+            return numeral(value).format('0,0') + ' vnd';
+        },
+        section: hbs_section(),
+
+    },
+}));
+
+app.set("view engine", "hbs");
+app.set("views", "./views");
+
+app.use(connectLiveReload());
+app.use(express.urlencoded({
+    extended: true,
+}));
+
+
+app.use('/account', accountRoute);
+
+app.listen(3000, function () {
+    console.log("Server started on http://localhost:3000");
+});
