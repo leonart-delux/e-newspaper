827d8755bda4eaabb53fdd88aa1a0003131393d6
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-12-16 22:33:19 +0700

    Update user middleware

--- middlewares/user.mdw.js
+++ middlewares/user.mdw.js
@@ -1,12 +1,21 @@
 import accountService from "../services/accountService.js";
 import moment from "moment/moment.js";
+import subscriberService from "../services/subscriberService.js";
+import writerService from "../services/writerService.js";
+import applyService from "../services/applyService.js";
 
 const userId = 5;
 
 export async function getVipUser(req, res, next) {
     //Tạm thời là lấy = userId sau này lấy bằng session
-    const user = await accountService.getVipStatus(userId);
+    let user = await subscriberService.getVipStatus(userId);
     user.birth_date = moment(user.birth_date).format("DD-MM-YYYY");
-    req.session.user = await accountService.getWriterPseudonym(user);
+    user = await writerService.getWriterPseudonym(user);
+    const userApply = await applyService.getPendingApplyUser(user.id);
+    if (userApply) {
+        user.roleStatus = 'pending';
+    }
+    req.session.user = user;
+
     next();
 }
