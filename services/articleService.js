5b826e33fa11af6180612b8ddcd046976db125ec
Bien Xuan Huy <bienxhuy@gmail.com>
2024-12-17 11:44:39 +0700

    add tag

--- services/articleService.js
+++ services/articleService.js
@@ -7,6 +7,8 @@ export default {
             .where({ writer_id: id, is_available: 1 })
             .join('articles_categories', 'articles.id', 'articles_categories.article_id')
             .join('categories', 'articles_categories.category_id', 'categories.id')
+            .join('articles_tags', 'articles.id', 'articles_tags.article_id')
+            .join('tags', 'articles_tags.tag_id', 'tags.id')
             .select(
                 'articles.id',
                 'articles.title',
@@ -14,7 +16,9 @@ export default {
                 'articles.main_thumb',
                 'articles.publish_date',
                 'categories.id as category_id',
-                'categories.name as category_name'
+                'categories.name as category_name',
+                'tags.id as tag_id',
+                'tags.name as tag_name',
             );
 
         // Group categories by article
@@ -27,14 +31,24 @@ export default {
                     abstract: row.abstract,
                     main_thumb: row.main_thumb,
                     is_published: row.publish_date < Date.now(),
-                    categories: []
+                    categories: [],
+                    tags: [],
                 };
             }
 
-            if (row.category_id && row.category_name) {
+            if (row.category_id && row.category_name && 
+                !articlesMap[row.id].categories.some(c => c.id === row.category_id)) {
                 articlesMap[row.id].categories.push({
                     id: row.category_id,
                     name: row.category_name
+                });
+            }
+
+            if (row.tag_id && row.tag_name && 
+                !articlesMap[row.id].tags.some(t => t.id === row.tag_id)) {
+                articlesMap[row.id].tags.push({
+                    id: row.tag_id,
+                    name: row.tag_name
                 });
             }
         });
@@ -50,6 +64,8 @@ export default {
             .whereIn('drafts.status', ['pending', 'rejected'])
             .leftJoin('articles_categories', 'articles.id', 'articles_categories.article_id')
             .leftJoin('categories', 'articles_categories.category_id', 'categories.id')
+            .leftJoin('articles_tags', 'articles.id', 'articles_tags.article_id')
+            .leftJoin('tags', 'articles_tags.tag_id', 'tags.id')
             .select(
                 'articles.id',
                 'articles.title',
@@ -57,7 +73,9 @@ export default {
                 'articles.main_thumb',
                 'drafts.status as status',
                 'categories.id as category_id',
-                'categories.name as category_name'
+                'categories.name as category_name',
+                'tags.id as tag_id',
+                'tags.name as tag_name',
             );
 
         // Group categories by article
@@ -74,10 +92,19 @@ export default {
                 };
             }
 
-            if (row.category_id && row.category_name) {
+            if (row.category_id && row.category_name && 
+                !articlesMap[row.id].categories.some(c => c.id === row.category_id)) {
                 articlesMap[row.id].categories.push({
                     id: row.category_id,
                     name: row.category_name
+                });
+            }
+
+            if (row.tag_id && row.tag_name && 
+                !articlesMap[row.id].tags.some(t => t.id === row.tag_id)) {
+                articlesMap[row.id].tags.push({
+                    id: row.tag_id,
+                    name: row.tag_name
                 });
             }
         });
