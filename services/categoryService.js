7137cc63bb91b4ce4e2723c4e7f0178485dc1227
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-12-17 11:07:34 +0700

    add method for categoryService.js

--- services/categoryService.js
+++ services/categoryService.js
@@ -4,4 +4,10 @@ export default{
     getAllCategories() {
         return db('categories').select('*');
     },
+    getChildCategories(catId) {
+        return db('categories').where('parent_id', catId);
+    },
+    getCategory(catId) {
+        return db('categories').where('id', catId).first();
+    },
 }
