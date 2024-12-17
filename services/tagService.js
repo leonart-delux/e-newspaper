fb3653c68dc1236aeb8dcf04f5cc9c2337107694
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-12-18 00:50:37 +0700

    add getTagNameById

--- services/tagService.js
+++ services/tagService.js
@@ -5,7 +5,10 @@ export default {
         //Only get tag_name
         return db('articles_tags').where('article_id', articleId)
             .join('tags', 'articles_tags.tag_id', 'tags.id')
-            .select('name');
-
+            .select('name', 'articles_tags.tag_id');
     },
+    getTagNameById(tagId) {
+        return db('tags').where('id', tagId).select('name as tagName').first();
+    },
+
 };
