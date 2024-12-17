d73cb57837d13cdbbb4d7be72e4f0cafc3a78b8b
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-12-18 00:49:57 +0700

    add getArticlesByTags...

--- services/articleService.js
+++ services/articleService.js
@@ -1,6 +1,7 @@
 import db from "../utils/db.js";
 import categoryService from "./categoryService.js";
 import tagService from "./tagService.js";
+import helper from "../utils/helper.js";
 
 export default {
     async getArticlesByCat(catId, limit, offset) {
@@ -8,10 +9,9 @@ export default {
         let articlesList = await db('articles')
             .where('articles_categories.category_id', catId).limit(limit).offset(offset)
             .join('articles_categories', 'articles.id', '=', 'articles_categories.article_id')
-            .select('id', 'title', 'abstract', 'main_thumb');
+            .select('id', 'title', 'abstract', 'main_thumb', 'articles.id as article_id', 'publish_date');
 
         // Với mỗi aricle thì thêm category names và tag names cho nó
-
         return this.getCatsAndTagsForAnArticle(articlesList);
     },
     async getCatsAndTagsForAnArticle(articlesList) {
@@ -21,6 +21,13 @@ export default {
             return article;
         }));
 
+        //Lười quá nên đây để xử lý time lun
+
+        articlesList = articlesList.map((article) => {
+            article.publish_date = helper.formatDateTime(article.publish_date)
+            return article;
+        });
+
         return articlesList;
     },
     countByCatId(catId) {
@@ -29,21 +36,52 @@ export default {
             .count('* as quantity')
             .first();
     },
+    countByTagId(tagId) {
+        return db('articles_tags')
+            .where('articles_tags.tag_id', tagId)
+            .count('* as quantity')
+            .first();
+    },
     async getArticlesByKeywords(keywords, limit, offset) {
         let articlesList = await db('articles')
             .whereRaw('MATCH(title,content,abstract) against (? in natural language mode)', [keywords])
             .limit(limit)
             .offset(offset)
-            .select('id', 'title', 'abstract', 'main_thumb', 'content');
+            .select('id', 'title', 'abstract', 'main_thumb', 'content', 'articles.id as article_id', 'publish_date');
 
-        articlesList =await this.getCatsAndTagsForAnArticle(articlesList);
+        articlesList = await this.getCatsAndTagsForAnArticle(articlesList);
         const count = await db('articles')
             .whereRaw('MATCH(title,content,abstract) against (? in natural language mode)', [keywords])
             .count('* as quantity').first();
 
         return {content: articlesList, count: count.quantity}
+    },
 
+    async getArticlesByTags(tagId, limit, offset) {
+        const articlesList = await db('articles')
+            .where('articles_tags.tag_id', tagId)
+            .limit(limit)
+            .offset(offset)
+            .join('articles_tags', 'articles.id', 'articles_tags.article_id')
+            .select('articles.id', 'title', 'abstract', 'main_thumb', 'content', 'articles.id as article_id', 'publish_date');
+        return this.getCatsAndTagsForAnArticle(articlesList);
+    },
 
+    async getNewestArticles(limit, offset) {
+        //Lấy tất cả các báo theo category ra
+        let articlesList = await db('articles')
+            .orderBy('publish_date', 'desc')
+            .limit(limit)
+            .offset(offset)
+            .select('id', 'title', 'abstract', 'main_thumb', 'articles.id as article_id', 'publish_date');
+
+        // Với mỗi aricle thì thêm category names và tag names cho nó
+        return this.getCatsAndTagsForAnArticle(articlesList);
+    },
+    count() {
+        return db('articles_tags')
+            .count('* as quantity')
+            .first();
     },
 
 }
