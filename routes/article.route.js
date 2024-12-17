ffbb4ae9f271a5ae8c9e65e7224af48ec39de5d8
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-12-17 15:53:58 +0700

    add pagination, articles list for page content

--- routes/article.route.js
+++ routes/article.route.js
@@ -1,5 +1,7 @@
 import express from "express";
 import categoryService from "../services/categoryService.js";
+import articleService from "../services/articleService.js";
+import tagService from "../services/tagService.js";
 
 const router = express.Router();
 
@@ -8,21 +10,43 @@ router.get('', function (req, res) {
         layout: 'home',
     });
 });
-router.get('/cat',async function (req, res) {
+router.get('/cat', async function (req, res) {
     const catId = +req.query.catId || 6;
+    const limit = 2;
+    const page = +req.query.page || 1;
+    const offset = (page - 1) * limit;
 
-    const childCats =await categoryService.getChildCategories(catId);
+    const childCats = await categoryService.getChildCategories(catId);
 
     const category = await categoryService.getCategory(catId);
 
-    console.log(childCats);
-    console.log(category);
+    const articlesByCat = await articleService.getArticlesByCat(catId, limit, offset);
 
+    const totalArticles = await articleService.countByCatId(catId);
 
-    res.render('vwHome/articleListByCat',{
+    const nPages = Math.ceil(totalArticles.quantity / limit);
+    const page_items = [];
+    for (let i = 1; i <= nPages; i++) {
+        page_items.push({
+            value: i,
+            active: i === +page,
+        })
+    }
+    console.log(page_items.length);
+
+    const prevPage = page === 1 ? page_items.length : page - 1;
+    const nextPage = page === page_items.length ? 1 : page + 1;
+    res.render('vwHome/articleListByCat', {
         layout: 'home',
         mainCat: category,
         childCats: childCats,
+        articles: articlesByCat,
+        empty: articlesByCat.length === 0,
+        page_items: page_items,
+        catId: catId,
+        prevPage: prevPage,
+        nextPage: nextPage,
+
     });
 });
 
