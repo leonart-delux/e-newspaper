da721d68c9e93d529071506f324474e4117e737e
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-12-18 00:50:26 +0700

    add format publish date

--- utils/helper.js
+++ utils/helper.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 export default {
     getSubscribeDate(outdatedDateTime) {
 // Chuyển đổi chuỗi thời gian thành đối tượng Date
@@ -6,4 +8,22 @@ export default {
         subscriptionDate.setDate(outdate.getDate() - 7);
         return subscriptionDate;
     },
+    formatDateTime(dateStr) {
+        moment.locale('vi');
+        const dateTime = moment(dateStr).format('h:mm a,dddd,LL')
+
+        const parts = dateTime.split(/,(.+)/);
+        const timeParts = parts[0].split(' ');
+        let dateParts = parts[1].split(',');
+        if (timeParts[1] === 'sa') {
+            timeParts[1] = 'Sáng';
+        } else {
+            timeParts[1] = 'Chiều';
+        }
+        let firstPart = dateParts[0].charAt(0).toUpperCase() + dateParts[0].slice(1);
+        let secondPart = dateParts[1].replace(/(\d+)(\s+)/, '$1$2');
+        console.log(secondPart);
+        return `${timeParts[0]} ${timeParts[1]}, ${secondPart}`;
+
+    }
 }
