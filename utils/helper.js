26a3b5250a74e418b94bd3566974ae5ca43e4fad
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-12-18 10:20:37 +0700

    add paginationVars function

--- utils/helper.js
+++ utils/helper.js
@@ -1,4 +1,5 @@
 import moment from "moment";
+import articleService from "../services/articleService.js";
 
 export default {
     getSubscribeDate(outdatedDateTime) {
@@ -25,5 +26,35 @@ export default {
         console.log(secondPart);
         return `${timeParts[0]} ${timeParts[1]}, ${secondPart}`;
 
-    }
+    },
+
+    //Đây là một call back function (hàm gọi hàm)
+    // getArticles và count là 2 hàm được truyền vào
+    // id là giá trị được sử dụng để tìm article (tag_id, keyword,cat_id)
+    async paginationVars(id, limit, offset, page, getArticles, count, urlType) {
+        const totalArticles = await count(id);
+        let articles = null;
+        if (urlType === 'newest') {
+            articles = await getArticles.bind(articleService)(limit, offset);
+        } else {
+            articles = await getArticles.bind(articleService)(id, limit, offset);
+        }
+        const nPages = Math.ceil(totalArticles.quantity / limit);
+        const page_items = [];
+        for (let i = 1; i <= nPages; i++) {
+            page_items.push({
+                value: i,
+                active: i === +page,
+            })
+        }
+        const prevPage = page === 1 ? page_items.length : page - 1;
+        const nextPage = page === page_items.length ? 1 : page + 1;
+        return {
+            page_items: page_items,
+            prevPage: prevPage,
+            nextPage: nextPage,
+            articles: articles
+        }
+    },
+
 }
