11e00a1631c7a86c4ba7d2d7df75c9b004aa0393
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-12-18 21:13:24 +0700

    Add route in admin

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -3,12 +3,44 @@ import express from "express";
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('vwAdmin/linechart',{
-        layout: 'home',
+    res.render('vwAdmin/linechart', {
+        layout: 'admin',
+        dashboard: true,
+
     });
 });
 
 router.get('/categories', function (req, res) {
+    res.render('vwAdmin/categories-menu', {
+        layout: 'admin',
+        categories: true,
+    });
+});
 
+router.get('/tags', function (req, res) {
+    res.render('vwAdmin/categories-menu', {
+        layout: 'admin',
+        tags: true,
+    });
+});
+
+router.get('/articles', function (req, res) {
+    res.render('vwAdmin/categories-menu', {
+        layout: 'admin',
+        articles: true,
+    });
+});
+router.get('/users', function (req, res) {
+    res.render('vwAdmin/categories-menu', {
+        layout: 'admin',
+        users: true,
+    });
+});
+
+router.get('/role-registers', function (req, res) {
+    res.render('vwAdmin/categories-menu', {
+        layout: 'admin',
+        role: true,
+    });
 });
 export default router;
