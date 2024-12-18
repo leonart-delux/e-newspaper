f7e06a6d076556ecc9132545e2cca1d69ea7f17d
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-12-18 22:33:27 +0700

    Add getAllCategoriesAndItsChildCat

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,6 +1,6 @@
 import db from "../utils/db.js";
 
-export default{
+export default {
     getAllCategories() {
         return db('categories').select('*');
     },
@@ -15,6 +15,20 @@ export default{
         return db('articles_categories').where('article_id', articleId)
             .join('categories', 'articles_categories.category_id', 'categories.id')
             .select('name', 'articles_categories.category_id');
+    },
+
+    async getAllCategoriesAndItsChildCat() {
+        const categories = await this.getAllCategories();
+
+        //Sử dụng map thay vì forEach do:
+        //forEach không chờ đợi từ promise xong rồi chạy tiếp, map thì có
+        // map cho phép dùng Promise.all để chờ đợi tất cả các promise hoàn thành rồi mới tiếp tục thực hiện cviec tiếp theo
+        const promises = categories.map(async (category) => {
+            const childCats = await this.getChildCategories(category.id);
+            category.childCats = childCats;
+            return category;
+        });
+        return await Promise.all(promises);
     },
 
 
