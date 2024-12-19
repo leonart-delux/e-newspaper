213f32948b642afb776e181597ad264f3d45c17f
“myuh250” <huyng2724@gmail.com>
2024-12-20 01:58:51 +0700

    Login with facebook

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as FacebookStrategy } from "passport-facebook";
 import { config } from "dotenv";
 import db from "../utils/db.js";
 config();
