b2c1b5698508b9520b82b33dcd4f5c1211e2e7d0
“myuh250” <huyng2724@gmail.com>
2024-12-19 16:02:51 +0700

    Login with FB ok to use

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -1,23 +1,25 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as FacebookStrategy } from "passport-facebook";
 import { config } from "dotenv";
 import db from "../utils/db.js";
 config();
 
 function generateRandomPassword(length = 16) {
-    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz'; 
-    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
-    const numericChars = '0123456789'; 
-    const specialChars = '!@#$%^&*()_+-=[]{}|;:,.<>?'; 
-    const allChars = lowercaseChars + uppercaseChars + numericChars + specialChars;
-    let password = '';
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * allChars.length);
-      password += allChars[randomIndex];
-    }
-    return password;
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numericChars = "0123456789";
+  const specialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+  const allChars =
+    lowercaseChars + uppercaseChars + numericChars + specialChars;
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars[randomIndex];
   }
+  return password;
+}
 
 passport.use(
   new GoogleStrategy(
@@ -51,6 +53,52 @@ passport.use(
     }
   )
 );
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: "http://localhost:3000/auth/facebook/callback",
+      profileFields: ["email", "id", "displayName"],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        if (profile?.id) {
+          const email = profile.emails?.[0]?.value;
+
+          if (email) {
+            const user = await db("users")
+              .where({ email })
+              .first();
+
+            if (!user) {
+              const fakeBcryptPassword = generateRandomPassword();
+              const hashFakePassword = await bcrypt.hash(fakeBcryptPassword, 8);
+              const [newUser] = await db("users")
+                .insert({
+                  email,
+                  password: hashFakePassword,
+                  role: "guest",
+                })
+                .returning("*");
+              return done(null, newUser);
+            }
+            return done(null, user);
+          } else {
+            return done(null, false, { message: "Tài khaorn không có email liên kết. Vui lòng đăng ký bằng email để tiếp tục sử dụng." });
+          }
+        } else {
+          return done(new Error("Không tìm thấy tài khoản."), null);
+        }
+      } catch (error) {
+        console.error("Error during OAuth authentication:", error);
+        return done(error, null);
+      }
+    }
+  )
+);
+
 
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
