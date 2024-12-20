9d5f4dfe6563107898c021240a3ad1b4a2c7417c
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-12-20 15:03:02 +0700

    add get-child-categories get in category router

--- routes/category.route.js
+++ routes/category.route.js
@@ -4,9 +4,38 @@ import categoryService from "../services/categoryService.js";
 
 const router = express.Router();
 
-router.get('/category/get-all-category-names', async function (req, res) {
-    let categories = await categoryService.getAllCategories()
+router.get('/get-all-category-names', async function (req, res) {
+    let categories = await categoryService.getAllNonParentCategories()
     return res.json(categories);
+});
+
+router.get('/get-child-categories', async function (req, res) {
+    const catId = +req.query.catId;
+
+    const childCats = await categoryService.getChildCategories(catId);
+
+    let categories = await categoryService.getAllCategories();
+
+    let nonParentcategories = await categoryService.getAllNonParentCategories();
+
+    console.log(childCats);
+    if (childCats.length !== 0) {
+        categories = categories.map((category => {
+            category.selected = childCats.some(childCat => childCat.id === category.id);
+            return category;
+        }));
+    }
+
+    console.log(catId);
+    categories= categories.filter((category) => {
+        return category.selected === true || nonParentcategories.some(nonParentCategory => nonParentCategory.id === category.id);
+    })
+    categories = categories.filter((category) => category.id !== catId);
+
+
+    return res.json(categories);
+
+
 });
 
 export default router;
