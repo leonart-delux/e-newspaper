91c23370a475bd30182fdc447b88d632893d308e
Bien Xuan Huy <bienxhuy@gmail.com>
2024-12-20 19:57:10 +0700

    add auth func check role for editor and writer

--- middlewares/auth.mdw.js
+++ middlewares/auth.mdw.js
@@ -7,15 +7,27 @@ export function isAuth(req, res, next) {
 }
 
 export function isWriter(req, res, next) {
-    // Chỉnh lại sau
-    // if (req.session.user.role !== 'writer') {
-    //     const script = `
-    //     <script>
-    //         alert('Bạn không có quyền truy cập vào trang web này.');
-    //         window.location.href = '/';
-    //     </script>
-    //     `;
-    //     return res.send(script);
-    // }
+    if (req.session.user.role !== 'writer') {
+        const script = `
+        <script>
+            alert('Bạn không có quyền truy cập vào trang web này.');
+            window.location.href = '/';
+        </script>
+        `;
+        return res.send(script);
+    }
+    next();
+}
+
+export function isEditor(req, res, next) {
+    if (req.session.user.role !== 'editor') {
+        const script = `
+        <script>
+            alert('Bạn không có quyền truy cập vào trang web này.');
+            window.location.href = '/';
+        </script>
+        `;
+        return res.send(script);
+    }
     next();
 }
