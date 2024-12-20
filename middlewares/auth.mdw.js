a29f76f1d7094966eea16e072c268e24b4d73e7f
Bien Xuan Huy <bienxhuy@gmail.com>
2024-12-20 21:29:07 +0700

    add auth function to check valid writer

--- middlewares/auth.mdw.js
+++ middlewares/auth.mdw.js
@@ -1,3 +1,5 @@
+import writerService from "../services/writerService.js";
+
 export function isAuth(req, res, next) {
     if (!req.session.auth) {
         req.session.retUrl = req.originalUrl;
@@ -14,7 +16,7 @@ export function isWriter(req, res, next) {
             window.location.href = '/';
         </script>
         `;
-        return res.send(script);
+        return res.status(403).send(script);
     }
     next();
 }
@@ -27,7 +29,28 @@ export function isEditor(req, res, next) {
             window.location.href = '/';
         </script>
         `;
-        return res.send(script);
+        return res.status(403).send(script);
+    }
+    next();
+}
+
+// When writer do an action, check if this writer has auth to do that action
+// It's like modifying another writer's article
+export async function isValidWriter(req, res, next) {
+    const article_id = +req.query.id || 0;
+    if (article_id === 0) {
+        return;
+    }
+
+    const articleOfWriter = await writerService.fetchArticleOfWriter(req.session.user.id, article_id);
+    if (articleOfWriter.length === 0) {
+        const script = `
+        <script>
+            alert('Bạn không có quyền truy cập vào trang web này.');
+            window.location.href = '/';
+        </script>
+        `;
+        return res.status(403).send(script);
     }
     next();
 }
