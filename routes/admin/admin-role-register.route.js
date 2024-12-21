f592117e975112660ee85c57de412ffa56dfa4ae
“myuh250” <huyng2724@gmail.com>
2024-12-22 04:03:04 +0700

    Done basic logic for admin interrect with role-register user

--- routes/admin/admin-role-register.route.js
+++ routes/admin/admin-role-register.route.js
@@ -1,12 +1,39 @@
 import express from "express";
+import applyService from '../../services/applyService.js';
+import userService from '../../services/accountService.js';
 
 const router = express.Router();
 
 router.get('/', async function (req, res) {
+    const writerApplications = (await applyService.getApplicationsByRole('writer')).reverse();
+    const editorApplications = (await applyService.getApplicationsByRole('editor')).reverse();
+
     res.render('vwAdmin/role-register/role-register-menu', {
         layout: 'admin',
         role: true,
+        writerApplications,
+        editorApplications,
+        writerRoleName: 'writer', 
+        editorRoleName: 'editor', 
     });
+});
+
+router.post('/', async function (req, res) {
+    const { id, status, role } = req.body;
+
+    try {
+        await applyService.updateStatus(id, status);
+
+        if (status === 'approved' && role) {
+            const application = await applyService.getApplicationById(id);
+            await userService.updateUser(application.user_id, { role });
+        }
+        res.json({ success: true, message: 'Cập nhật thành công!' });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ success: false, message: 'Đã xảy ra lỗi!' });
+    }
 });
 
 export default router;
--- services/applyService.js
+++ services/applyService.js
@@ -4,7 +4,22 @@ export default {
     addApplyRole(entity) {
         return db('apply').insert(entity);
     },
+
     getPendingApplyUser(userId) {
         return db('apply').where('user_id', userId).where('status', 'pending').first();
     },
+
+    getApplicationById(id) { 
+        return db('apply').where('id', id).first();
+    },
+
+    getApplicationsByRole(role) {
+        return db('apply')
+            .select('id', 'user_id', 'self_access', 'status', 'role')
+            .where('role', role);
+    },
+
+    updateStatus(id, status) {
+        return db('apply').where('id', id).update('status', status);
+    },  
 }
